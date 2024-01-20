8b860cc9591af6efb5485aa9898be8efa3cf235a
AJMAL MT <ajmalmt67@gmail.com>
2024-01-20 17:00:08 +0530

    about page completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,21 @@
 const About = () => {
-	return <h1 className="text-4xl font-bold underline">About</h1>;
+	return (
+		<>
+			<div className="flex flex-wrap gap-2 sm:gap sm:gap-x-6 items-center justify-center">
+				<h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+					We love Regal
+				</h1>
+			</div>
+			<p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+				Welcome to Regle Woods, where every piece of furniture tells a
+				story of craftsmanship and sophistication. Immerse yourself in a
+				world of timeless elegance as we bring you a curated collection
+				of handcrafted furniture. Explore our range and transform your
+				space into a haven of comfort and beauty. Your home, your style
+				– Regle Woods, where furniture meets finesse.
+			</p>
+		</>
+	);
 };
 
 export default About;
